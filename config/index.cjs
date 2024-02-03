904931b63e9968cd9b65a46537710f1f8d3bdc49
Magical <100467733+shaoao@users.noreply.github.com>
2024-02-03 22:25:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,6 +5,8 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
+
+
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
@@ -29,11 +31,11 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '2003', date: '09-15',
+          type: '*生日', name: '蕊蕊', year: '2003', date: '09-15',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '奥奥', year: '2002', date: '11-28',
         },
         {
           type: '节日', name: '相爱纪念日', year: '2023', date: '02-16',
@@ -51,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	o_BS49V60yzobTUo3dGL4QRJGhrTQ_2qCLdnv8Q-AUc',
+  CALLBACK_TEMPLATE_ID: 'o_BS49V60yzobTUo3dGL4QRJGhrTQ_2qCLdnv8Q-AUc',
 
   CALLBACK_USERS: [
     {
