401ba83cbe72a677c5449ff6d464c888d478ae16
Magical <100467733+shaoao@users.noreply.github.com>
2024-02-04 01:16:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '蕊蕊',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ohUw26d8__3xKKQVID0QMUQ_wOI4',
+      id: 'ohUw26U8Hz6WrLzAnA1PTJ1YtTNc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'g-ORIJFkpfZ2SkuDtgdaU00Zyc--V2HAvmj2gjqoawo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
